22b0938dd0ecb4358fd17b6501196f7e456387f0
Carlos Garcia <carloseduardogarher@gmail.com>
2024-02-21 20:50:13 -0600

    Practica 4
    Practica 4 Completa

--- myscript.js
+++ myscript.js
@@ -1,44 +1,37 @@
 var total=0;
 
-function login() {
-    var Usuario = document.getElementById("Usuario");
-    let mensajeE = document.querySelector('.mensajeE');
-    let userinput = document.createElement("p");
-    mensajeE.removeChild(mensajeE.firstChild);
-    userinput.textContent=("Inicio Sesion: " + Usuario.value);
-    mensajeE.appendChild(userinput);
-    console.log("Inicio Sesion: ", Usuario.value);
-    document.getElementById("Usuario").value = "";
-
-}
-
-function register() {
-    var Usuario = document.getElementById("Usuario");
-    let mensajeE = document.querySelector('.mensajeE');
-    let userinput = document.createElement("p");
-    mensajeE.removeChild(mensajeE.firstChild);
-    userinput.textContent=("Registro Exitoso: " + Usuario.value);
-    mensajeE.appendChild(userinput);
-    console.log("Registro Exitoso: ", Usuario.value);
-    document.getElementById("Usuario").value = "";
-}
-
 function additem(){
     var Producto = document.getElementById("Producto");
     var Precio = document.getElementById("Precio");
-    total=total+parseFloat(Precio.value);
-    var ProductoN=Producto.value;
-    console.log(total + "  -  " + ProductoN);
+    if( Producto.value!== "" && Precio.value!== ""){
+        total=total+parseFloat(Precio.value);
+        var ProductoN=Producto.value;
+        console.log(total + "  -  " + ProductoN);
+    
+        var carrito = document.querySelector(".carritoE");
+        var prodcutoE = document.createElement('div');
+        prodcutoE.setAttribute("class","productoE");
+        var productoID = document.createElement("div");
+        productoID.setAttribute("class", "productoDesc");
+        var productoPR = document.createElement("div");
+        productoPR.setAttribute("class", "productoPrec");
+    
+        var IDtxt = document.createElement("p");
+        var PRtxt = document.createElement("p");
+        IDtxt.textContent=(ProductoN);
+        PRtxt.textContent=parseFloat(Precio.value);
+        PRtxt.textContent=("$" + PRtxt.textContent);
+        productoID.appendChild(IDtxt);
+        productoPR.appendChild(PRtxt);
+        prodcutoE.appendChild(productoID);
+        prodcutoE.appendChild(productoPR);
+        carrito.appendChild(prodcutoE);
+    
+        document.getElementById("Producto").value = "";
+        document.getElementById("Precio").value = "";
 
-    var resumen = document.querySelector('.Resumen');
-    var productotxt = document.createElement("p");
-    var preciotxt = document.createElement("p");
-    var totaltxt = document.createElement("p");
-    productotxt.textContent=(ProductoN + " - $");
-    preciotxt.textContent=parseFloat(Precio.value);
-    totaltxt.textContent=(total);
-    resumen.appendChild(productotxt);
-    resumen.appendChild(preciotxt);
-    resumen.appendChild(totaltxt);
+        var totalcarrito=document.getElementById("total");
+        totalcarrito.textContent=("Total: $" + total + " MXN");
+    }
     
 }
