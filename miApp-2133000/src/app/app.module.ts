5a6989190eaa16ccfe39ac373c96d483e98b6763
Carlos Garcia <carloseduardogarher@gmail.com>
2024-03-20 10:20:41 -0600

    Practica 7

--- miApp-2133000/src/app/app.module.ts
+++ miApp-2133000/src/app/app.module.ts
@@ -7,10 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ListaAlumnosComponent} from './lista-alumnos/lista-alumnos.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,
+    ListaAlumnosComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  
 })
 export class AppModule {}
