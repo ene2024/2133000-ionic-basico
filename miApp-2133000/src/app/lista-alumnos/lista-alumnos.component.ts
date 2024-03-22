53ea4c4ed4972dd8fbfded6cee3bada23f1bdfd3
Carlos Garcia <carloseduardogarher@gmail.com>
2024-03-21 22:14:56 -0600

    Practica 8

--- miApp-2133000/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-2133000/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,16 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.scss'],
 })
+
 export class ListaAlumnosComponent  implements OnInit {
 
-  constructor() { }
+  constructor(public actionSheetController: ActionSheetController) { }
 
   ngOnInit() {}
 
   alumnos: any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
 
+
+async mostraractionsheet(index:number){
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Confirmar Eliminacion',
+      
+      buttons: [
+        {
+          text: 'Eliminar',
+          handler: () => {
+            this.eliminaralumno(index);
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelado');
+          }
+        }
+      ]
+    });
+
+    await actionSheet.present();
+    
+  }
+
+  
+  eliminaralumno(index: number) {
+    this.alumnos.splice(index, 1);
+  }
+
 }
+
+
